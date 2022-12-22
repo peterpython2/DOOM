bda9f673a649e5bc5f983b881f9ee712ce7e0235
peterpython2 <jjjnt2017@gmail.com>
2022-12-22 09:42:47 -0500

    \

--- PycharmProjects/pythonProject/object_renderer.py
+++ PycharmProjects/pythonProject/object_renderer.py
@@ -2,11 +2,21 @@ import pygame as pg
 from settings import *
 
 
-  def __init__(self, game):
+class ObjectRenderer:
+    def __init__(self, game):
         self.game = game
         self.screen = game.screen
+        self.wall_textures = self.load_wall_textures()
 
-  @staticmethod
+    @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pg.image.load(path).convert_alpha()
         return pg.transform.scale(texture, res)
+
+    def load_wall_textures(self):
+        return {
+            1: self.get_texture('resources/textures/tex-1.png'),
+            2: self.get_texture('resources/textures/tex-2.png'),
+            3: self.get_texture('resources/textures/tex-3.png'),
+
+            }

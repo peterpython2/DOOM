835794050df59c67b10b90ad47e1b5dbc3361513
peterpython2 <jjjnt2017@gmail.com>
2023-01-09 09:41:04 -0500

    \

--- PycharmProjects/pythonProject/object_renderer.py
+++ PycharmProjects/pythonProject/object_renderer.py
@@ -8,6 +8,13 @@ class ObjectRenderer:
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
 
+    def draw(self):
+        self.render_game_objects()
+    def render_game_objects(self):
+        list_objects = self.game.raycasting.object_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pg.image.load(path).convert_alpha()
@@ -15,8 +22,8 @@ class ObjectRenderer:
 
     def load_wall_textures(self):
         return {
-            1: self.get_texture('resources/textures/tex-1.png'),
-            2: self.get_texture('resources/textures/tex-2.png'),
-            3: self.get_texture('resources/textures/tex-3.png'),
+            1: self.get_texture('Resources/Textures/tex-1.png'),
+            2: self.get_texture('Resources/Textures/tex-2.png'),
+
 
             }

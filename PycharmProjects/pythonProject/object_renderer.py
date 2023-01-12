4ebe14a36357647d947bf8bde7c1bdef60a7353b
peterpython2 <jjjnt2017@gmail.com>
2023-01-11 23:48:48 -0500

    \

--- PycharmProjects/pythonProject/object_renderer.py
+++ PycharmProjects/pythonProject/object_renderer.py
@@ -18,7 +18,7 @@ class ObjectRenderer:
         pg.draw.rect(self.screen, CEILING_COLOR, (0, 0, WIDTH, HALF_HEIGHT))
 
     def render_game_objects(self):
-        list_objects = self.game.raycasting.object_to_render
+        list_objects = sorted(self.game.raycasting.object_to_render, key=lambda t: t[0], reverse=True)
         for depth, image, pos in list_objects:
             self.screen.blit(image, pos)
 

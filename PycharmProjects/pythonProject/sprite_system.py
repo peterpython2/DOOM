7c11fd41ca7e8fa01a42b149c47303fffd0d9dab
peterpython2 <jjjnt2017@gmail.com>
2023-01-12 19:27:02 -0500

    \

--- PycharmProjects/pythonProject/sprite_system.py
+++ PycharmProjects/pythonProject/sprite_system.py
@@ -54,7 +54,7 @@ class SpriteSystem:
 
 class AnimatedSprite(SpriteSystem):
     def __init__(self, game, path='Resources/Sprites/NPC/death/0.png',
-                 pos=(10, 3.5), scale=0.8, shift=0.15, animation_time=120):
+                 pos=(11.5, 3.5), scale=0.8, shift=0, animation_time=120):
         super().__init__(game, path, pos, scale, shift)
         self.animation_time = animation_time
         self.path = path.rsplit('/', 1)[0]
@@ -77,7 +77,7 @@ class AnimatedSprite(SpriteSystem):
         time_now = pg.time.get_ticks()
         if time_now - self.animation_time_prev > self.animation_time:
             self.animation_time_prev = time_now
-            self.animation_trigger = False
+            self.animation_trigger = True
 
     def get_images(self, path):
         images = deque()
@@ -86,4 +86,3 @@ class AnimatedSprite(SpriteSystem):
                 img = pg.image.load(path + '/' + file_name).convert_alpha()
                 images.append(img)
         return images
-
